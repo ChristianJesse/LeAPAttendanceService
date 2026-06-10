e40a0e652576ecc33177f7a9d6e4ab777f380712
COOreto <COOreto@neltex.com>
2026-06-10 15:15:16 +0800

    Added new model class for retrieving config data

---
+++ LeAPAttendanceService/LeAPAttendanceService/Models/LeapConfigValue.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+namespace LeAPAttendanceService.Models;
+
+
+public sealed class LeapConfigValue
+{
+    public string Category { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public int IsActive { get; set; }   
+}
+
+
